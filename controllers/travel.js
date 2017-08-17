a0de941db9cb5128a74d11f9cb81fea5aa369304
Mark Murphy <mark@murphymark.me>
2017-08-17 16:12:32 -0500

    filled in controller file

--- controllers/travel.js
+++ controllers/travel.js
@@ -1,3 +1,30 @@
 const express = require('express');
 const router = express.Router();
 const Travel = require ('../models/travel.js');
+
+router.get('/', (req,res)=>{
+  Travel.find({}, (err, foundTravel)=>{
+    res.json(foundTravel);
+  });
+});
+
+router.post('/', (req, res)=> {
+  console.log(req.body)
+    Travel.create(req.body, (err, createdTravel)=> {
+      res.json(createdTravel)
+    });
+});
+
+router.delete('/:id', (req,res)=>{
+  Travel.findByIdAndRemove(req.params.id, (err, deletedTravel)=> {
+    res.json(deletedTravel)
+  });
+});
+
+router.put('/:id', (req,res)=> {
+  Travel.findByIdAndUpdate(req.params.id, req.body, {new:true}, (err, updatedTravel)=> {
+     res.json(updatedTravel);
+  });
+});
+
+module.exports = router;
