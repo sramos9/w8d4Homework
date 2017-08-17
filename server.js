29dcdf41c21ea8b1794991fcdb4b335d8864ce69
Mark Murphy <mark@murphymark.me>
2017-08-17 15:50:16 -0500

    set up initial directories and app.js/server.js/index.html

--- server.js
+++ server.js
@@ -1,9 +1,19 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
+app.use(express.static('public'));
 
 app.get('/', (req,res)=>{
   res.send('Hello World')
 })
+
+mongoose.connect('mongodb://localhost:27017/travel');
+mongoose.connection.once('open', ()=>{
+    console.log('connected to mongo');
+});
 
 app.listen(3000, function(){
     console.log('listening...');
