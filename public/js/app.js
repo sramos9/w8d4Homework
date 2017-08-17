3854f4fae85542ff1b22f2529a40914e639601af
Samantha Ramos <samantharamos@samanthas-air.attlocal.net>
2017-08-17 16:50:25 -0500

    update and delete routes created

--- public/js/app.js
+++ public/js/app.js
@@ -8,7 +8,7 @@ app.controller('travelController', ['$http', function($http){
       url: '/travel'
     }).then(
       function(response){
-        console.log('this is the response');
+        console.log('this is the GET response', response);
         controller.todos = response.data
       },
       function(error){
@@ -26,6 +26,7 @@ app.controller('travelController', ['$http', function($http){
       }
     }).then(
       function(response){
+        console.log('this is the CREATE response', response);
         controller.getTravelInfo();
         //this should make info show on page immediately, no refresh.
       },
@@ -34,6 +35,35 @@ app.controller('travelController', ['$http', function($http){
       }
     );
   }
-  
+  this.editTravelInfo = function(travelInfo){
+    $http({
+      method: 'PUT',
+      url: '/travel/' + travel._id,
+      data: {
+        description: travel.description,
+        image: travel.image
+      }
+    }).then(
+      function(response){
+        controller.getTravelInfo();
+      },
+      function(error){
+
+      }
+    );
+  }
+  this.deleteTravelInfo = function(travelInfo){
+    $http({
+      method: 'DELETE',
+      url: '/travel/' + travel._id
+    }).then(
+      function(response){
+        controller.getTravelInfo();
+      },
+      function(error){
+
+      }
+    );
+  }
   this.getTravelInfo();
 }])
